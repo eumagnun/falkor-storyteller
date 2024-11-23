be2d05dc2bfd1e3331dc22978f7aecde6e2233cf
Daniel <eumagnun@gmail.com>
2024-11-23 19:59:32 -0300

    ajustes tela de configuracao

--- pages/Setup.py
+++ pages/Setup.py
@@ -1,7 +1,34 @@
-﻿import google.generativeai as genai
-import streamlit as st
-from Home_config import LLM_API_KEY,LLM_MODEL
-from services.service_file import load_world
+﻿import streamlit as st
+from services.service_file import load_world_file
+from services.service_memory import __generate_file_name
 
 st.set_page_config(page_title="Falkor",)
 st.title("Falkor - Setup")
+
+st.subheader("Configurar opções") 
+
+dados = load_world_file()  
+reino_selecionado = st.selectbox(
+    "Selecione o Reino",
+    [k for k in dados['kingdoms']],
+     format_func=lambda k: k['name']
+)
+
+st.info(reino_selecionado['description'])
+
+cidade_selecionada = st.selectbox(
+    "Selecione a cidade",
+    [t for t in reino_selecionado['towns']],
+    format_func=lambda t: t['name']
+)
+st.info(cidade_selecionada['description'])
+
+npc_selecionada = st.selectbox(
+    "Selecione a NPC",
+    [n for n in cidade_selecionada['npcs']],
+    format_func=lambda n: n['name']
+)
+st.info(npc_selecionada['description'])
+
+if st.button("Reiniciar estória com opções atuais?"):
+    print("reset")
--- services/service_file.py
+++ services/service_file.py
@@ -1,8 +1,23 @@
 ﻿
 import json
+import os 
 
 def load_world(filename):
     with open(filename, 'r') as f:
         return json.load(f)
     
 
+def load_world_file():
+    # Obtém o caminho absoluto da pasta atual (pasta A)
+    pasta_atual = os.path.dirname(os.path.abspath(__file__))
+
+    # Constrói o caminho para a pasta B (pasta irmã)
+    pasta_b = os.path.join(pasta_atual, "../mundos_criados")
+
+    # Constrói o caminho completo para o arquivo JSON (ajuste o nome do arquivo)
+    caminho_arquivo = os.path.join(pasta_b, "2024-11-23T095943Z_mundo_criado.json")
+
+    # Abre o arquivo e lê o conteúdo JSON
+    with open(caminho_arquivo, 'r',encoding='utf8') as arquivo:
+        dados_json = json.load(arquivo)
+        return dados_json
